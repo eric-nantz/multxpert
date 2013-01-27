4a3ca4d3084e3922bb12aac0f8da6d9a9d3c7882
Eric Nantz <eric.nantz@gmail.com>
2013-01-27 06:38:05 -0500

    Added script to build package with devtools

---
+++ package_builder.R
@@ -0,0 +1,22 @@
+#-------------------------------------------------------
+# script using devtools package to compile package
+#-------------------------------------------------------
+
+library(devtools)
+
+# turn on dev_mode
+
+dev_mode()
+
+# load all function files
+load_all("~/Dropbox/rpodcast_code/multxpert/")
+
+# use this to generate manual files from R script files with Roxygen style 
+document("~/Dropbox/rpodcast_code/multxpert/")
+
+# check a package can be bult
+check("~/Dropbox/rpodcast_code/multxpert/")
+
+# turn off dev_mode
+
+dev_mode()\
